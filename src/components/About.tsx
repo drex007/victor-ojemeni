136de2865bc489c10aa12c57dc7e3b25597e06b8
drex007 <vykeoj@gmail.com>
2025-04-03 11:02:55 +0100

    feat: updated about me and banner

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,7 +49,7 @@ const About = () => {
                   {inView ? <CountUp start={0} end={7} duration={10} /> : null}+
                 </div>
                 <div className='font-primary text-sm tracking-[2px] text-white'>
-                  Tech<br />Stack/Frameworks
+                  Tech<br /><span className='text-[12px] lg:text-sm'>Stack/Frameworks</span>
                 </div>
               </div>
 
