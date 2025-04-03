b9d8bdb16a38ef6146b5adadcfd6b285fe4d7cc7
drex007 <vykeoj@gmail.com>
2025-04-03 11:00:12 +0100

    feat: updated my cv

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -10,11 +10,11 @@ import fadeIn from '../Variant'
 const services = [
   {
     name: "Backend developer",
-    description: "Proficient in backend frameworks like django, nodejs, fastapi, solidity, django rest framework and have built projects using these frameworks",
+    description: "Proficient in backend frameworks like django, nodejs, fastapi, solidity, django rest framework and have built projects using these technologies",
   },
   {
     name: "Frontend developer",
-    description: "Proficient in javascript and javascript frameworks like reactjs, typsecript and have built projects using these frameworks"
+    description: "Proficient in javascript and javascript frameworks like reactjs, next js, typsecript and have built projects using these technologies"
   },
   {
     name: "Mobile app developer",
@@ -22,7 +22,7 @@ const services = [
   },
   {
     name: "Blockchain developer",
-    description: "I use solidity for my blockchain development, ethers and web3 js to integrate my backend code to my frontend.Deployed live solidity contracts on the EVM network and won block magic hackthon on devpost",
+    description: "I use solidity for my blockchain development, ethers and web3 js to integrate my backend code to my frontend.Deployed live solidity contracts on the EVM network and won block magic hackathon",
   },
   {
     name: "Technical writer",
@@ -49,7 +49,7 @@ const Services = () => {
             className='flex-1 lg:bg-bottom bg-no-repeat mb-12 lg:mb-0'>
             <h2 className='h2  mb-6 text-white'>What I Do</h2>
 
-            <h3 className='h3 max-w-[455px] mb-8 text-white'>I am a freelance with over 4 years of working experience</h3>
+            <h3 className='h3 max-w-[455px] mb-8 text-white'>I am a freelancer with over 4 years of working experience</h3>
             <p className='text-white font-semibold text-[15px]'>
               {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry.
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s */}
